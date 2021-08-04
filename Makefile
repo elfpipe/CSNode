5a0e727e317637d5cbd71a59c843628616598eda
alfkil <me@github.com>
2021-08-04 10:44:16 +0200

    Server3

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 all: CSNode
 
-CSNode: CSNode.hpp CSNode.cpp Buffer.cpp Strings.hpp Server2.cpp main3.cpp
-	g++ main3.cpp CSNode.cpp Server2.cpp -pthread -o CSNode
+CSNode: CSNode.hpp CSNode.cpp Buffer.cpp Strings.hpp Server3.cpp main3.cpp
+	g++ main3.cpp CSNode.cpp Server3.cpp -pthread -o CSNode
 
 clean:
 	rm CSNode
---
+++ Server3.cpp
@@ -0,0 +1,25 @@
+#include <unistd.h>
+#include <poll.h>
+#include <iostream>
+#include <strings.h>
+#include "CSNode.hpp"
+#include "Server.hpp"
+Server::Server (CSNode *node) {
+    this->node = node;
+}
+void Server::startThread () {
+    cout << "Accepting calls...\n";
+    CSNode::CSConnection *connection = node->waitForIncomming (node->port);
+    if (connection) {
+        cout << "Incomming call(!)...\n";
+        // cout << "<message> : " << _this->node->readSentence (connection, '\3') << "\n";
+        // _this->node->writeSentence (connection, "CLOSE");
+        node->serverCommand (connection);
+        node->closeConnection (connection);
+        cout << "Call completed.\n";
+    }
+}
+void *Server::thread(void *dummy) {
+}
+void Server::endThread() {
+}
